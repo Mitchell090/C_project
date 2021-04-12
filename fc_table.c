c16d5e117a9e90f3433ca4115971319d51ca4084
Mitchell090 <mitchell2basketball@gmail.com>
2021-04-12 20:02:57 +1000

    new fc table

--- fc_table.c
+++ fc_table.c
@@ -3,8 +3,8 @@
  for fahr = 0, 20, ..., 300 */
 main()
 {
-    int fahr, celsius;
-    int lower, upper, step;
+    float fahr, celsius;
+    float lower, upper, step;
 
     lower = 0;  // lower limit of temperature
     upper = 300; // upper limit
@@ -13,8 +13,8 @@ main()
     fahr = lower;
     while (fahr <= upper)
     {
-        celsius = 5*(fahr-32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f %6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
