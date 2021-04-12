85f705ba4c12b34de754f72ecc0392ed3f21a9ea
Mitchell090 <mitchell2basketball@gmail.com>
2021-04-12 18:41:14 +1000

    fc_table yep

--- fc_table.c
+++ fc_table.c
@@ -4,5 +4,17 @@
 main()
 {
     int fahr, celsius;
-    int lower, upper, ster;
+    int lower, upper, step;
+
+    lower = 0;  // lower limit of temperature
+    upper = 300; // upper limit
+    step = 20; // step size
+
+    fahr = lower;
+    while (fahr <= upper)
+    {
+        celsius = 5*(fahr-32) / 9;
+        printf("%d\t%d\n", fahr, celsius);
+        fahr = fahr + step;
+    }
 }
